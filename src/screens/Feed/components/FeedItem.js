0b2147d7d18ef4dead7037a7ef0fe493c5eb1921
luispuig <mail@lpuig.com>
2018-01-27 16:41:37 +0100

    FeedList / ItemInfo / Format Date

--- src/screens/Feed/components/FeedItem.js
+++ src/screens/Feed/components/FeedItem.js
@@ -1,25 +1,30 @@
 import React from "react";
 
 import { Text, View, TouchableOpacity } from "react-native";
+import TimeAgo from "react-native-timeago";
 
 class FeedItem extends React.PureComponent {
-  _selectItem = () => {
-    // Call parent selectItem with item argument
-    const { selectItem, item } = this.props;
-    selectItem(item);
+  _selectItem = () => this.props.selectItem(this.props.item); // Call parent selectItem with item argument
+
+  // TODO: Move time calcs to utils lib
+  _EpochToDate = epoch => {
+    if (epoch < 10000000000) epoch *= 1000; // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
+    epoch = epoch + new Date().getTimezoneOffset() * -1; //for timeZone
+    return new Date(epoch);
   };
 
   render() {
     const { item } = this.props;
-    const {id, title, author, num_comments, score, created_utc} = item.data;
-    
+    const { id, title, author, num_comments, score, created_utc } = item.data;
+    const date = this._EpochToDate(created_utc); // timestamp format.
+
     return (
       <View>
         <TouchableOpacity onPress={this._selectItem}>
           <View>
             <Text>{id}</Text>
             <Text>{title}</Text>
-            <Text>date: {created_utc}</Text>
+            <Text>date: <TimeAgo time={date} /></Text>
             <Text>{author}</Text>
             <Text>{num_comments} comments</Text>
             <Text>score: {score}</Text>
