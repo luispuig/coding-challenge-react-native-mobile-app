d380b803e2e5b8202941477fc7bbc11b209457dc
luispuig <mail@lpuig.com>
2018-01-28 00:21:39 +0100

    Tests / DetailScreen

---
+++ src/screens/Detail/DetailScreen.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import ShallowRenderer from "react-test-renderer/shallow"; // ES6
+
+import DetailScreen from "./DetailScreen";
+
+const props = {
+    navigation: {
+      state: {
+        params: {
+          title: "title",
+          url: "http://example.com"
+        }
+      }
+    }
+  };
+
+
+it("match on Shallow mode", () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<DetailScreen {...props} />);
+  const tree = renderer.getRenderOutput();
+  expect(tree).toMatchSnapshot();
+});
+
+it('it should apply navigation title', () => {
+    const navigationOptions = DetailScreen.navigationOptions(props);
+    expect(navigationOptions).toMatchSnapshot();
+  });
+
+
